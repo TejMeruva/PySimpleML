3a592b7c16185c2764cd3dacbd64553a4c0f0455
Teja Meruva <msreeramulu@Keys.local>
2025-08-12 15:02:23 +0530

    Added scores.py

--- PySimpleML/scores.py
+++ PySimpleML/scores.py
@@ -1,19 +1,92 @@
 import pandas as pd
 import numpy as np
 
-def confusionMatrix(y, target):
+def _mapping(y:pd.Series, target:pd.Series):
+    ynp = y.to_numpy().reshape(-1, 1)
+    targetnp = target.to_numpy().reshape(-1, 1)
+    ytar = np.concatenate((ynp, targetnp), axis=1)
+    mapping = {v:k for k, v in dict(enumerate(np.unique(ytar))).items()}
+    return mapping
 
+def confusionMatrix(y:pd.Series, target:pd.Series):
+    ynp = y.to_numpy()
+    targetnp = target.to_numpy()
+    ytar = np.column_stack((y, target))
+    mapping = _mapping(y, target)
+    cm = np.zeros((len(mapping), len(mapping)), dtype=int)
+    def map(x):
+        return mapping[x]
+    map = np.vectorize(map)
+    ytar = map(ytar)
+    def update(col):
+        cm[col[0], col[1]] += 1
+        return 0
+    np.apply_along_axis(update, axis=1, arr=ytar)
+    return cm
+
+def _TP(y, target, tarLabel):
+    mapping = _mapping(y, target)
+    ind = mapping[tarLabel]
+    cm = confusionMatrix(y, target)
+    return cm[ind, ind]
+
+def _TN(y, target, tarLabel):
+    mapping = _mapping(y, target)
+    ind = mapping[tarLabel]
+    cm = confusionMatrix(y, target)
+    return cm[:ind, :ind].sum() + cm[ind+1:, ind+1:].sum() + cm[ind+1:, :ind].sum() + cm[:ind, ind+1:].sum()
+
+def _FP(y, target, tarLabel):
+    mapping = _mapping(y, target)
+    ind = mapping[tarLabel]
+    cm = confusionMatrix(y, target)
+    return cm[:ind, ind].sum() + cm[ind+1:, ind].sum()
+
+def _FN(y, target, tarLabel):
+    mapping = _mapping(y, target)
+    ind = mapping[tarLabel]
+    cm = confusionMatrix(y, target)
+    return cm[ind, :ind].sum() + cm[ind, ind+1:].sum()
+
+def _evalCM(y, target, tarLabel, cm):
+    mapping = _mapping(y, target)
+    ind = mapping[tarLabel]
+    return {
+        "TP": cm[ind, ind],
+        "TN": cm[:ind, :ind].sum() + cm[ind+1:, ind+1:].sum() + cm[ind+1:, :ind].sum() + cm[:ind, ind+1:].sum(),
+        "FP": cm[:ind, ind].sum() + cm[ind+1:, ind].sum(), 
+        "FN": cm[ind, :ind].sum() + cm[ind, ind+1:].sum()
+    }
 def recallScore(y, target):
-    pass
+    #tp/(tp + fn)
+    op = dict()
+    mapping = _mapping(y, target)
+    for tarLabel in mapping:
+        evaln = _evalCM(y, target, tarLabel, confusionMatrix(y, target))
+        tp, fn = evaln["TP"], evaln["FN"]
+        if tp + fn == 0:
+            op[tarLabel] = 0
+        else:
+            op[tarLabel] = tp / (tp + fn)
+    return pd.Series(op)
 
 def accuracyScore(y, target):
     return (y == target).sum()/y.shape[0]
 
 def precisionScore(y, target):
-    pass
+    op = dict()
+    mapping = _mapping(y, target)
+    for tarLabel in mapping:
+        evaln = _evalCM(y, target, tarLabel, confusionMatrix(y, target))
+        tp, fp = evaln["TP"], evaln["FP"]
+        if tp + fp == 0:
+            op[tarLabel] = 0
+        else:
+            op[tarLabel] = tp / (tp + fp)
+    return pd.Series(op)
 
 def f1Score(y, target):
-    pass
+    return 2 * (precisionScore(y, target) * recallScore(y, target)) / (precisionScore(y, target) + recallScore(y, target)) if (precisionScore(y, target) + recallScore(y, target)).sum() != 0 else pd.Series({k: 0 for k in _mapping(y, target)})
 
 def RSMEScore(y, target):
     y = y.to_numpy()
--- test2.py
+++ test2.py
@@ -1,8 +1,15 @@
-from PySimpleML.scores import accuracyScore
+from PySimpleML.scores import confusionMatrix, _TP, _TN, _FP, recallScore, precisionScore, f1Score
 import pandas as pd
 
-items = ['Pen', 'Pencil', 'Eraser', 'Sharpener']
-items = pd.Series(items)
-items2 = items.sample(frac=1, replace=True).reset_index(drop=True)
-print(pd.concat([items, items2], axis=1))
-print(accuracyScore(items.reset_index(drop=True), items2.reset_index(drop=True)))
+data = {
+    "Feature1": [5.1, 4.9, 6.2, 5.9, 6.5, 5.0, 6.0, 5.5, 6.3, 5.8],
+    "Feature2": [3.5, 3.0, 3.4, 3.2, 3.0, 3.6, 3.1, 3.5, 2.9, 3.3],
+    "Feature3": [1.4, 1.4, 4.5, 4.2, 5.5, 1.4, 4.0, 1.3, 5.6, 4.4],
+    "Class":    ["A", "A", "B", "B", "C", "A", "B", "A", "C", "B"]
+}
+
+df = pd.DataFrame(data)
+a =  df["Class"].sample(frac=1)
+print(confusionMatrix(df["Class"], a))
+# print(_FP(df["Class"], a, "B"))
+print(f1Score(df["Class"], a))
