b902acc9b74470f805074e0ba2dc6fe5e644a2a3
Teja Meruva <msreeramulu@Keys.local>
2025-08-19 00:36:50 +0530

    Added Feature Importances to RF.py and DT.py

---
+++ Examples/RF/RFClassification.py
@@ -0,0 +1,36 @@
+from PySimpleML.models.RF import RandomForest
+
+from PySimpleML.scores import f1Score, precisionScore, accuracyScore
+import pandas as pd
+# from PySimpleML.models.DT import DecisionTree as DT1
+# from PySimpleML.models.DT import DecisionTree as DT2
+
+data = pd.read_csv('/Users/msreeramulu/SWD/Python/PySimpleML/Examples/data/Flowers.csv')
+dataShuff = data.sample(frac=1).rename(columns={'Species':'Label'}).reset_index(drop=True)
+# print(data.head())
+# dataShuff.drop(['Id'], inplace=True)
+trainData = dataShuff.iloc[:140, :]
+testData = dataShuff.iloc[140:, :]
+# print(data.info())
+Xtrain = trainData.iloc[:, 1:-1]
+ytrain = trainData.iloc[:, [-1]]
+Xtest = testData.iloc[:, 1:-1]
+ytest = testData.iloc[:, -1]
+# print(Xtest.head())
+
+#DT1
+# tree = DT1(pd.concat([Xtrain, ytrain], axis=1), 1)
+# # tree.train(Xtrain, ytrain)
+# pred = tree.predict(Xtest)
+# print(f1Score(pd.Series(pred), ytest))
+# print(tree)
+
+#DT2 (10 times faster exp)
+forest = RandomForest(100, 3, 1)
+forest.train(Xtrain, ytrain)
+pred = forest.predict(Xtest)
+print(f1Score(pred, ytest))
+forest.getFeatureImportances().plot(kind='barh')
+import matplotlib.pyplot as plt
+plt.show()
+# print(tree)
